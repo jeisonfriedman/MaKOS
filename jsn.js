5f7863a5e045fc42d58fda84faa1814617c699df
jeisonfriedman <51206897+jeisonfriedman@users.noreply.github.com>
2020-06-18 00:04:34 -0500

    Update and rename jsn0.js to jsn.js

--- jsn.js
+++ jsn.js
@@ -2,16 +2,15 @@
 // Al cargar la página
 document.addEventListener('DOMContentLoaded', function() {
     // Declaraciones
-    const url = '//raw.githubusercontent.com/jeisonfriedman/makos/master/jsn.css';
+    const style = '//raw.githubusercontent.com/jeisonfriedman/makos/master/control.css';
     const head = document.querySelector('head');
     const table = document.getElementById('tablepucs');
     const div = document.createElement('div');
     // Si existe la tabla
     if (table) {
         // Creo y agrego link
-        div.innerHTML = `<link href="${url}" rel="stylesheet">`;
+        div.innerHTML = `<link href="${style}" rel="stylesheet">`;
         head.appendChild(div.children[0]);
     }
-    console.log(div.innerHTML);
     console.log(table);
 })

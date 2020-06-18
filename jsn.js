6930079abf76a793ef1a657f7c8ab525af6d2d1a
jeisonfriedman <51206897+jeisonfriedman@users.noreply.github.com>
2020-06-17 23:37:06 -0500

    Update jsn.js

--- jsn.js
+++ jsn.js
@@ -1,12 +1,16 @@
 'use strict';
-// Declaraciones
-const head = document.getElementsByTagName('head')[0];
-const table = document.getElementById('tablepucs');
-const div = document.createElement('div');
-// Si existe la tabla
-if (table) {
-    // Creo y agrego link
-    div.innerHTML = `<link href="${repUrl}" rel="stylesheet">`;
-    head.appendChild(div.children[0]);
-}
-console.log(table);
+alert('Jsn');
+// Al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+    // Declaraciones
+    const head = document.getElementsByTagName('head')[0];
+    const table = document.getElementById('tablepucs');
+    const div = document.createElement('div');
+    // Si existe la tabla
+    if (table) {
+        // Creo y agrego link
+        div.innerHTML = `<link href="${repUrl}" rel="stylesheet">`;
+        head.appendChild(div.children[0]);
+    }
+    console.log(table);
+})

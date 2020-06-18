417e6b541a457fb99312ff865c4e7cca842bf427
jeisonfriedman <51206897+jeisonfriedman@users.noreply.github.com>
2020-06-17 23:10:21 -0500

    Add files via upload

---
+++ jsn.js
@@ -0,0 +1,12 @@
+'use strict';
+// Declaraciones
+const head = document.getElementsByTagName('head')[0];
+const table = document.getElementById('tablepucs');
+const div = document.createElement('div');
+// Si existe la tabla
+if (table) {
+    // Creo y agrego link
+    div.innerHTML = `<link href="${repUrl}" rel="stylesheet">`;
+    head.appendChild(div.children[0]);
+}
+console.log(table);

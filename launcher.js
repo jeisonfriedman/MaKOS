23561380b3c36179b9c88d31a8bd745204ba40ed
JeisonF <jeisonfriedman@gmail.com>
2020-06-19 15:55:34 -0500

    Update

--- launcher.js
+++ launcher.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         MaKOS Cloud Fixes
 // @namespace    https://app.makos.com.co/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       Jsn'
-// @updateURL    https://jeisonfriedman.github.io/MaKOS/launcher.js
 // @match        https://app.makos.com.co/*
 // @grant        none
 // @require      https://jeisonfriedman.github.io/MaKOS/jsn.js
+// @updateURL    https://jeisonfriedman.github.io/MaKOS/launcher.js
+// @downloadURL  https://jeisonfriedman.github.io/MaKOS/launcher.js
 // ==/UserScript==
 
 (function() {

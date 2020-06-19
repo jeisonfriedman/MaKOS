9266fea7a46e9950da27c00e2f84944c632e9186
JeisonF <jeisonfriedman@gmail.com>
2020-06-19 15:38:35 -0500

    Update

---
+++ script.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         MaKOS Cloud Fixes
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Jsn'
+// @match        https://app.makos.com.co/*
+// @grant        none
+// @require      https://jeisonfriedman.github.io/MaKOS/jsn.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();

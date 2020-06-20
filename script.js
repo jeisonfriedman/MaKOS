af73114fb1e00b3c7c4c0f49fa93cb302e0f6c55
JeisonF <jeisonfriedman@gmail.com>
2020-06-20 11:48:19 -0500

    Update

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name         MaKOS Cloud Fixes
 // @namespace    https://app.makos.com.co/
-// @version      0.5
+// @version      0.1
 // @description  Ajustes de MaKOS Cloud
 // @author       Jeison Friedman
 // @match        https://app.makos.com.co/*
 // @run-at       document-start
 // @grant        none
-// @require      https://jeisonfriedman.github.io/MaKOS/jsn.js
 // @updateURL    https://jeisonfriedman.github.io/MaKOS/script.js
 // @downloadURL  https://jeisonfriedman.github.io/MaKOS/script.js
+// @require      https://jeisonfriedman.github.io/MaKOS/jsn.js
 // ==/UserScript==
-// (function() {
-//     'use strict';
-// })();

c8c3ffce75c9bd31affa27a6b236945b00d51bbf
JeisonF <jeisonfriedman@gmail.com>
2020-06-20 10:25:32 -0500

    Update

--- jsn.js
+++ jsn.js
@@ -2,36 +2,41 @@
 // @grant        none
 // ==/UserScript==
 'use strict';
-// Declaraciones
-const url = '//jeisonfriedman.github.io/MaKOS/';
-// Al cargar la página
-document.addEventListener('DOMContentLoaded', function() {
-    // Aplico estilos
-    applyStyles();
-    // Ajusto ortografía
-    // correctSpelling();
-})
+// Clase
+class Jsn {
+    constructor() {
+        // Propiedades
+        this.url = '//jeisonfriedman.github.io/MaKOS/';
+        // Al cargar la página
+        document.addEventListener('DOMContentLoaded', function() {
+            // Aplico estilos
+            this.applyStyles();
+            // Ajusto ortografía
+            // correctSpelling();
+        })
+    }
 
-// Aplica estilos
-function applyStyles() {
-    // Declaraciones
-    const div = document.createElement('div');
-    const head = document.querySelector('head');
-    const table = document.getElementById('tablepucs');
-    // Si existe la tabla
-    if (table) {
-        // Creo y agrego link        
-        div.innerHTML = `<link href="${url}/controls.css" type="text/css" rel="stylesheet">`;
-        head.appendChild(div.children[0]);
+    // Aplica estilos
+    applyStyles() {
+        // Declaraciones
+        const div = document.createElement('div');
+        const head = document.querySelector('head');
+        const table = document.getElementById('tablepucs');
+        // Si existe la tabla
+        if (table) {
+            // Creo y agrego link        
+            div.innerHTML = `<link href="${this.url}/controls.css" type="text/css" rel="stylesheet">`;
+            head.appendChild(div.children[0]);
+        }
+    }
+
+    // Corrijo ortografía
+    correctSpelling() {
+        // Declaraciones
+        const item = document.querySelector('#main-menu-navigation > li:nth-child(5) > a > span');
+        // Menú principal
+        if (item.innerHTML === 'Tesoreria') item.innerHTML = 'Tesorería';
     }
 }
-
-// Corrijo ortografía
-function correctSpelling() {
-    // Declaraciones
-    const item = document.querySelector('#main-menu-navigation > li:nth-child(5) > a > span');
-    item.style.display = 'none';
-    console.log('item hidden');
-    // Menú principal
-    if (item.innerHTML === 'Tesoreria') item.innerHTML = 'Tesorería';
-}
+// Instancio clase
+const jsn = new Jsn();
